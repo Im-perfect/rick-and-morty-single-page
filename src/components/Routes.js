5cf261bb1358e6fe0f6aec4addec6e0536945559
Im-perfect <zhangke.kerri@gmail.com>
2020-03-08 12:51:29 +0100

    Added router for a given location

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import DimensionList from "./DimensionList";
 import LocationList from "./LocationList";
+import LocationDetails from './LocationDetails'
 import EpisodeList from "./EpisodeList";
 import CharacterList from "./CharacterList";
 
@@ -11,6 +12,7 @@ export default class Routes extends Component {
       <div>
         <Route path="/dimensions" exact component={DimensionList} />
         <Route path="/locations" exact component={LocationList} />
+        <Route path="/locations/:locationId" exact component={LocationDetails} />
         <Route path="/episodes" exact component={EpisodeList} />
         <Route path="/characters" exact component={CharacterList} />
       </div>
