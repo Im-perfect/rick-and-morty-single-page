6ddcf3c917e80cd37bc3466294190836b8b5120e
Im-perfect <zhangke.kerri@gmail.com>
2020-03-09 21:25:24 +0100

    Add home and navigation style (responsive)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,15 +2,60 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export default class Navigation extends Component {
+  state = {
+    selected: "home"
+  };
+
+  handleClick = selected => {
+    this.setState({
+      selected
+    });
+  };
+
   render() {
     return (
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/dimensions">Dimension</Link>
-        <Link to="/locations">Location</Link>
-        <Link to="/episodes">Episodes</Link>
-        <Link to="/characters">Characters</Link>
-      </div>
+      <ul className="nav">
+        <li
+          className={`nav-item ${
+            this.state.selected === "home" ? "is-active" : null
+          }`}
+          onClick={() => this.handleClick("home")}
+        >
+          <Link to="/">Home</Link>
+        </li>
+        <li
+          className={`nav-item ${
+            this.state.selected === "dimensions" ? "is-active" : null
+          }`}
+          onClick={() => this.handleClick("dimensions")}
+        >
+          <Link to="/dimensions">Dimensions</Link>
+        </li>
+        <li
+          className={`nav-item ${
+            this.state.selected === "locations" ? "is-active" : null
+          }`}
+          onClick={() => this.handleClick("locations")}
+        >
+          <Link to="/locations">Locations</Link>
+        </li>
+        <li
+          className={`nav-item ${
+            this.state.selected === "episodes" ? "is-active" : null
+          }`}
+          onClick={() => this.handleClick("episodes")}
+        >
+          <Link to="/episodes">Episodes</Link>
+        </li>
+        <li
+          className={`nav-item ${
+            this.state.selected === "characters" ? "is-active" : null
+          }`}
+          onClick={() => this.handleClick("characters")}
+        >
+          <Link to="/characters">Characters</Link>
+        </li>
+      </ul>
     );
   }
 }
